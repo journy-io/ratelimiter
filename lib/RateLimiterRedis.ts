3566c8af764495790c12b1cfeb2167f646f0c194
Hans Ott <hansott@hotmail.be>
2021-12-14 16:53:17 +0100

    ⚡️ Use defineCommand for lua scripts
    Closes #1

--- lib/RateLimiterRedis.ts
+++ lib/RateLimiterRedis.ts
@@ -3,13 +3,26 @@ import { RateLimitedResource } from "./RateLimitedResource";
 import { RateLimiter } from "./RateLimiter";
 import { Redis } from "ioredis";
 
-// TODO: Use definedCommand, which uses EVALSHA for better perf
-export class RateLimiterRedis implements RateLimiter {
-  constructor(private readonly redis: Redis, private readonly clock: Clock) {}
+interface RedisWithCommands extends Redis {
+  rateLimiterRemaining(
+    key: string,
+    nowInMilliseconds: number,
+    windowInMilliseconds: number,
+    allowedCalls: number
+  ): Promise<number>;
+  rateLimiterConsume(
+    key: string,
+    nowInMilliseconds: number,
+    windowInMilliseconds: number,
+    allowedCalls: number
+  ): Promise<number>;
+}
 
-  async remaining(rateLimitedItem: RateLimitedResource): Promise<number> {
-    return await this.redis.eval(
-      `
+export class RateLimiterRedis implements RateLimiter {
+  constructor(private readonly redis: Redis, private readonly clock: Clock) {
+    this.redis.defineCommand("rateLimiterRemaining", {
+      numberOfKeys: 1,
+      lua: `
         local token = KEYS[1]
         local now = tonumber(ARGV[1])
         local window = tonumber(ARGV[2])
@@ -22,7 +35,31 @@ export class RateLimiterRedis implements RateLimiter {
 
         return limit - amount
       `,
-      1,
+    });
+    this.redis.defineCommand("rateLimiterConsume", {
+      numberOfKeys: 1,
+      lua: `
+        local token = KEYS[1]
+        local now = tonumber(ARGV[1])
+        local window = tonumber(ARGV[2])
+        local limit = tonumber(ARGV[3])
+
+        local clearBefore = now - window
+        redis.call('ZREMRANGEBYSCORE', token, 0, clearBefore)
+
+        local amount = redis.call('ZCARD', token)
+        if amount < limit then
+            redis.call('ZADD', token, now, now)
+        end
+        redis.call('EXPIRE', token, window)
+
+        return limit - amount
+      `,
+    });
+  }
+
+  async remaining(rateLimitedItem: RateLimitedResource): Promise<number> {
+    return await (this.redis as RedisWithCommands).rateLimiterRemaining(
       this.getKey(rateLimitedItem),
       this.clock.getUTCTime().toMillis(),
       rateLimitedItem.getWindow().as("millisecond"),
@@ -40,25 +77,9 @@ export class RateLimiterRedis implements RateLimiter {
   }
 
   async consume(rateLimitedItem: RateLimitedResource): Promise<boolean> {
-    const remaining = await this.redis.eval(
-      `
-        local token = KEYS[1]
-        local now = tonumber(ARGV[1])
-        local window = tonumber(ARGV[2])
-        local limit = tonumber(ARGV[3])
-
-        local clearBefore = now - window
-        redis.call('ZREMRANGEBYSCORE', token, 0, clearBefore)
-
-        local amount = redis.call('ZCARD', token)
-        if amount < limit then
-            redis.call('ZADD', token, now, now)
-        end
-        redis.call('EXPIRE', token, window)
-
-        return limit - amount
-      `,
-      1,
+    const remaining = await (
+      this.redis as RedisWithCommands
+    ).rateLimiterConsume(
       this.getKey(rateLimitedItem),
       this.clock.getUTCTime().toMillis(),
       rateLimitedItem.getWindow().as("millisecond"),
